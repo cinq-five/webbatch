bad85dd0c597cb1f1054ff18dc360b6fdcd4f76f
Hervé Mutombo <herve@herocoders.com>
2022-08-29 14:01:36 +0100

    Added a few comments

--- batch.go
+++ batch.go
@@ -11,6 +11,8 @@ func (batch *Batch) AddStep(step Step) *Batch {
 	return batch
 }
 
+// Executes every step in the batch queue.
+// Does not execute subsequent steps to one that returns false.
 func (batch *Batch) Execute(writer http.ResponseWriter, request *http.Request, steps ...Step) bool {
 	batch.steps = append(batch.steps, steps...)
 
@@ -23,6 +25,8 @@ func (batch *Batch) Execute(writer http.ResponseWriter, request *http.Request, s
 	return true
 }
 
+// Executes every step in the batch queue.
+// Does not execute subsequent steps to one that returns false.
 func Execute(writer http.ResponseWriter, request *http.Request, steps ...Step) bool {
 	batch := Batch{
 		steps: steps,
